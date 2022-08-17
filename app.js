c4e006d4707d493c44173f20d0f741ba3e274966
nasanchez7 <nadirblanco02@gmail.com>
2022-08-17 19:54:20 -0300

    add register user

--- app.js
+++ app.js
@@ -29,20 +29,28 @@ const passwordForm = document.getElementById("password");
 
 const error = document.getElementById("error");
 
-//Eventos
-
-const button = document.getElementById("login");
-
-button.addEventListener("click", validateForm);
-
 
 //Validacion
 
 function validateForm(){
     
+    let checkUser = localStorage.getItem('localUser');
+
+    let checkUserObject = JSON.parse(checkUser);
+
+    users.push(checkUserObject);
+
+    console.log(checkUserObject);
+    console.log(users);
 
     const userSolicited = users.filter((user => user.name == userForm.value))
     
+    if(userForm.value == "" || passwordForm.value == ""){
+        error.innerHTML =`
+                <h4>Ingrese un usuario o contraseña</h4> 
+            `
+    }
+
     for(let user of userSolicited){
 
         console.log(user.name);
@@ -64,11 +72,40 @@ function validateForm(){
                 <h4>Usuario o contraseña incorrectos</h4> 
             `
         }
-
     }
-
 }
 
 
 
+//Registro de nuevo usuario
 
+const userRegister = document.getElementById("user-register");
+const passwordRegister = document.getElementById("password-register");
+
+const errorRegister = document.getElementById("error-register");
+
+class User {
+
+    constructor(name,password) {
+        this.name = name;
+        this.password = password;
+    }
+
+}
+
+function registerUser(){
+    console.log("click");
+
+    if(userRegister.value == "" || passwordRegister.value == ""){
+        errorRegister.innerHTML =`<h4>Ingrese un usuario o contraseña correctos</h4> `
+    }else{
+        const newUser = new User (userRegister.value, passwordRegister.value);
+        console.log(newUser);
+        errorRegister.innerHTML =`<h4 class="userCreate">Usuario creado exitosamente</h4>`
+
+        let userCreate = JSON.stringify(newUser);
+        localStorage.setItem('localUser', userCreate);
+    }
+
+
+}
